d9dfeecc8553b859a6472a6eafb37e0ec124abda
butschster <butschster@gmail.com>
2016-05-25 17:44:12 +0300

    rename backend middleware

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,12 +1,12 @@
 <?php
 
-Route::group(['prefix' => backend_url_segment(), 'as' => 'backend.plugins.', 'middleware' => ['web', 'backend.auth']], function () {
+Route::group(['prefix' => backend_url_segment(), 'as' => 'backend.plugins.', 'middleware' => ['backend']], function () {
     Route::get('plugins', ['as' => 'list', 'uses' => 'PluginController@getIndex']);
     Route::get('plugins/settings/{plugin}', ['as' => 'settings.get', 'uses' => 'PluginController@getSettings']);
     Route::post('plugins/settings/{plugin}', ['as' => 'settings.post', 'uses' => 'PluginController@postSettings']);
 });
 
-Route::group(['middleware' => ['web', 'api', 'backend.auth']], function () {
+Route::group(['middleware' => ['api', 'backend']], function () {
     RouteAPI::get('plugins', ['as' => 'api.plugins.list', 'uses' => 'API\PluginController@getList']);
     RouteAPI::post('plugins', ['as' => 'api.plugins.post', 'uses' => 'API\PluginController@changeStatus']);
 });
